0e228255d33cb03123b0494d416b4527f63d7847
Henrik Karlsson <hk.henrik@gmail.com>
2018-07-15 14:53:27 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const uuidv4 = require('uuid/v4')
+
+const requestLogger = log => async (ctx, next) => {
+  const start = Date.now()
+  const { request, response } = ctx
+
+  const clientRequestId = ctx.request.headers['x-request-id']
+  const requestId = clientRequestId ? `c-${clientRequestId}` : `s-${uuidv4()}`
+  ctx.log = log.child({ requestId })
+
+  await next()
+
+  const time = Date.now() - start
+
+  ctx.log.debug(
+    { request, response, time },
+    `${request.method} ${request.url} ${response.status}`,
+  )
+}
+
+module.exports = requestLogger
